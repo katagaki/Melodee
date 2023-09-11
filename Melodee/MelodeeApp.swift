a6a1b8cf09c429554af6ad4d36120176262209cb
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-11 14:54:27 +0800

    Initial Commit

---
+++ Melodee/MelodeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodeeApp.swift
+//  Melodee
+//
+//  Created by シン・ジャスティン on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct MelodeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

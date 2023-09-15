b91e0ce1bfe69ec2172f15b7f0c7210f86784131
katagaki <68231035+katagaki@users.noreply.github.com>
2023-09-15 18:07:37 +0800

    Use app name when file is in root folder

--- Melodee/Structs/FSFile.swift
+++ Melodee/Structs/FSFile.swift
@@ -26,7 +26,23 @@ struct FSFile: FilesystemObject {
     }
 
     func containingFolderName() -> String {
-        let url = URL(filePath: containingFolderPath())
-        return url.lastPathComponent
+        do {
+            let url = URL(filePath: containingFolderPath())
+            if url.lastPathComponent == "Documents" {
+                let documentsDirectoryURL = try FileManager.default
+                .url(for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true)
+                if documentsDirectoryURL != url {
+                    return url.lastPathComponent
+                }
+            } else {
+                return url.lastPathComponent
+            }
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+        return Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? ""
     }
 }
